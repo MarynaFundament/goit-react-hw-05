e7ed47834ee13508d572e2146fc7f1032b4c5908
MarynaFundament <marina.fundament@ukr.net>
2024-03-14 12:21:17 +0200

    rgrt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes} from "react-router-dom"
+import { Outlet, Route, Routes} from "react-router-dom"
 import Navigation from "./Navigation/Navigation";
 
 import HomePage from "../pages/HomePage";
@@ -16,14 +16,16 @@ import MovieDetailsPage from "../pages/MovieDetailsPage";
 
             <Route path = "/" element={<HomePage/>} />
             {/* <Route path = "/movies" element={<MoviesPage/>}  /> */}
-            <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-
-            {/* <Route path = "*" element={ <div> Oops! Not found!</div> } /> */}
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+         <Route path="subpage-a" element = {<div> Subpage A</div>}/>
+         <Route path="subpage-b"  element = {<div> Subpage B</div>}/>
+              </Route>
+            <Route path = "*" element={ <div> Oops! Not found!</div> } />
 
         </Routes>
  
   
-
+        <Outlet/>
     </div>
   );
 };
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useParams, NavLink } from "react-router-dom"
 import { useState, useEffect } from "react";
 
 import { getTrendsById } from "./trends-api";
@@ -28,15 +28,36 @@ export default function MovieDetailsPage() {
         }
         getDataById()
      
-    }, []);
+    }, [movieId]);
     
     return <div>
         <div>Movies DetailedPage : {movieId}
         </div>
 
-        <div> 
-            <p>{details.title}</p>
-        </div>
+        {details &&  <div> 
+            <h1>{details.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/w500${details.poster_path}`} alt={details.title} />
+            <p> User Score: {details.popularity}</p>
+            <h2>Overview</h2>
+            <p>{details.overview}</p>
+            <h2>Genres</h2>
+            {/* <ul>
+                        {details.genres.map(genre => (
+                            <li key={genre.id}>{genre.name}</li>
+                        ))}
+                    </ul> */}
+        </div>}
+
+        <ul>
+            <li>
+                <NavLink to = "subpage-a"> Subpage A</NavLink>
+            </li>
+            <li>
+                <NavLink to = "subpage-b">Subpage B</NavLink>
+            </li>
+        </ul>
+
+       
          </div>
 
 
